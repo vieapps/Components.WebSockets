6f357c44f9743e9959ef3cb1f4a7ce7fb611cd57
Quynh Nguyen <mr.quynh@gmail.com>
2020-03-10 10:12:39 +0700

    (improv): add comments

--- WebSocketOpCode.cs
+++ WebSocketOpCode.cs
@@ -2,11 +2,34 @@
 {
 	public enum WebSocketOpCode
 	{
+		/// <summary>
+		/// Continuous message
+		/// </summary>
 		Continuation = 0,
+
+		/// <summary>
+		/// Text message
+		/// </summary>
 		Text = 1,
+
+		/// <summary>
+		/// Binary message
+		/// </summary>
 		Binary = 2,
+
+		/// <summary>
+		/// Closing message
+		/// </summary>
 		ConnectionClose = 8,
+
+		/// <summary>
+		/// Ping message
+		/// </summary>
 		Ping = 9,
+
+		/// <summary>
+		/// Pong message
+		/// </summary>
 		Pong = 10
 	}
 }
